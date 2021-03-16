81afbe1b36e2ded151993e7bf969e3e9aaa24070
Chris Eldon <chris_eldon@yahoo.com>
2021-03-16 19:12:32 +0700

    profile page

--- app/src/main/java/com/chriseldon/unjukrasa/profile/ProfilePageFragment.kt
+++ app/src/main/java/com/chriseldon/unjukrasa/profile/ProfilePageFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chriseldon.unjukrasa.R
 import com.chriseldon.unjukrasa.databinding.FragmentProfilePageBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+
 
 class ProfilePageFragment : Fragment() {
 
@@ -25,6 +31,23 @@ class ProfilePageFragment : Fragment() {
 
         val binding = fragmentBinding!!
 
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.text) {
+                    resources.getString(R.string.mendukung) -> binding.rvDemonstration.adapter = DemonstrationListAdapter(arrayListOf("1111", "2222"))
+                    resources.getString(R.string.membuat) -> binding.rvDemonstration.adapter = DemonstrationListAdapter(arrayListOf("1111"))
+                    resources.getString(R.string.ditandai) -> binding.rvDemonstration.adapter = DemonstrationListAdapter(arrayListOf())
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+        binding.rvDemonstration.apply {
+            this.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            this.adapter = DemonstrationListAdapter(arrayListOf("1111", "2222"))
+        }
     }
 
     override fun onDestroyView() {
