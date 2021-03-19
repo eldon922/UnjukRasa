7ca9aa45898047fa883e16820411cd14cea69fa4
Chris Eldon <chris_eldon@yahoo.com>
2021-03-19 13:06:30 +0700

    login page

---
+++ app/src/main/java/com/chriseldon/unjukrasa/login/LoginPageFragment.kt
@@ -0,0 +1,35 @@
+package com.chriseldon.unjukrasa.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.chriseldon.unjukrasa.databinding.FragmentLoginPageBinding
+
+
+class LoginPageFragment : Fragment() {
+
+    private var fragmentBinding: FragmentLoginPageBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentBinding = FragmentLoginPageBinding.inflate(inflater, container, false)
+        return fragmentBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = fragmentBinding!!
+
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
+    }
+}
