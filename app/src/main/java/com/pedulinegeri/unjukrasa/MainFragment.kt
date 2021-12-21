4de2fd635764137132a89cfc8f238455abbb2be5
Chris Eldon <chris_eldon@yahoo.com>
2021-12-21 15:47:50 +0700

    fix safetynet and some ui

--- app/src/main/java/com/pedulinegeri/unjukrasa/MainFragment.kt
+++ app/src/main/java/com/pedulinegeri/unjukrasa/MainFragment.kt
@@ -14,6 +14,7 @@ import androidx.activity.addCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -33,7 +34,7 @@ class MainFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val mainViewModel: MainViewModel by viewModels()
-    private val authViewModel: AuthViewModel by viewModels()
+    private val authViewModel: AuthViewModel by activityViewModels()
 
     private lateinit var onBackPressedCallback: OnBackPressedCallback
     private lateinit var addOnBackPressedCallbackJob: Job
--- app/src/main/java/com/pedulinegeri/unjukrasa/UnjukRasaApplication.kt
+++ app/src/main/java/com/pedulinegeri/unjukrasa/UnjukRasaApplication.kt
@@ -1,7 +1,20 @@
 package com.pedulinegeri.unjukrasa
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class UnjukRasaApplication : Application()
+class UnjukRasaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseApp.initializeApp(/*context=*/this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
+    }
+}
